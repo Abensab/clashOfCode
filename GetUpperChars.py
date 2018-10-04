15b158d04e5effd1c431dd31f756e08bd3a07a95
Abraham Benitez Sabao <al292072@uji.es>
2018-10-04 12:26:36 +0200

    Update GetUpperChars.py

--- GetUpperChars.py
+++ GetUpperChars.py
@@ -1,11 +1,22 @@
 import sys
 import math
 
-s = raw_input()
-string=""
-for character in s:
-    if(character.lower()!=character.upper()):
-        if(character==character.upper()):
+#My code:
+def poor_code():
+    s = raw_input()
+    string=""
+    for character in s:
+        if(character.lower()!=character):
             string+=character
 
-print string
+    print(string)
+
+#Better code (RedDead):
+def better_code():
+    s = input()
+    b = [el for el in s if el.lower() != el] 
+    print("".join(b))
+    
+#Bettest code (Me, after seeing the code of RedDead):
+def bettest_code():
+    print("".join([c for c in input() if c.lower() != c]))
